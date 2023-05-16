07b81d20896ae2032b6cb08e01d420bb09962d62
SebHoet <114983975+SebHoet@users.noreply.github.com>
2023-05-16 11:50:58 +0200

    Add files via upload

--- scripts/Pcanvas.js
+++ scripts/Pcanvas.js
@@ -27,15 +27,18 @@ class Jugador {
     this.position.y += this.velocidad.y
     if (this.position.y >= positionSuelo) {
         this.position.y = positionSuelo
+        booleanos.salto.permitido = true
         
     } else if (this.position.y < 100) {
         this.position.y = 100
         if (booleanos.salto.permitido) {
             booleanos.salto.permitido = false;
-            setTimeout(() => {
+            /**
+             * setTimeout(() => {
                 booleanos.salto.permitido = true
             }, 
-            750);
+            400);
+             */
         }
     }
 
@@ -176,7 +179,8 @@ const teclas = {
 const booleanos = {
     salto:{
         permitido: true
-    }
+    },
+    choque: false
 }
 const propiedades = {
     jugador: {
@@ -186,7 +190,9 @@ const propiedades = {
         velocidad_inicio :2,
         velocidad_actual: 2,
         velocidad_maxima:10
-    }
+    },
+    puntuacion:0,
+    puntuacion_record: 0
     
 }
 
@@ -200,19 +206,25 @@ animate()
 window.addEventListener('keydown', (event) =>{
     switch (event.key) {
         case 'w':
+        case 'ArrowUp':
+        case ' ':
             teclas.arriba.pressed=true
             
 
             break;
         case 'a':
+        case 'ArrowLeft':
+            
             teclas.left.pressed=true
 
             break;
         case 'd':
+            case 'ArrowRight':
             teclas.right.pressed=true
     
             break;
         case 's':
+            case 'ArrowDown':
             teclas.abajo.pressed=true
                 break;
     }
@@ -221,17 +233,23 @@ window.addEventListener('keydown', (event) =>{
 window.addEventListener('keyup', (event) =>{
     switch (event.key) {
         case 'w':
+        case 'ArrowUp':
+        case' ':
             teclas.arriba.pressed=false
+            booleanos.salto.permitido = false;
             break;
         case 'a':
+        case 'ArrowLeft':
             teclas.left.pressed=false
     
                 break;
         case 'd':
+        case 'ArrowRight':
             teclas.right.pressed=false
         
                 break;
         case 's':
+        case 'ArrowDown':
             teclas.abajo.pressed=false
             break;
     }
@@ -243,13 +261,14 @@ window.addEventListener('keyup', (event) =>{
 async function moreObstaculos(){
       
         if (obstaculos.length <=8 && fechanow < Date.now()) {
+            propiedades.puntuacion +=Math.floor(10*propiedades.objetos.velocidad_actual)
             fechanow = Date.now()+750-(propiedades.objetos.velocidad_actual*20)
             let numRandom = Math.random();
            if (numRandom > 0.5 - (propiedades.objetos.velocidad_actual*0.025)) {
             
              obstaculos.push(new Obstaculo({
                 position:   {
-                x: 400+(Math.random()*200+10) -(propiedades.objetos.velocidad_actual*5) ,
+                x: 400+(Math.random()*200+20) -(propiedades.objetos.velocidad_actual*1) ,
                 y: 250
                 },
                 velocidad: {
@@ -268,13 +287,59 @@ async function moreObstaculos(){
    
 }
 //
+function perder(){
+    propiedades.objetos.velocidad_actual = propiedades.objetos.velocidad_inicio
+    propiedades.puntuacion = 0
+    obstaculos.length = 0
+    obstaculos.length = 1
+    obstaculos.push(new Obstaculo({
+        position:   {
+        x: 400,
+        y: 250
+        },
+        velocidad: {
+            x: 2,
+            y: 0
+        },
+        tamano:   {
+            x: 30,
+            y: 50
+        }
+    }))
+}
+
+function chocar(){
+    //console.log(jugador.position.x+" - "+(jugador.position.x+jugador.tamano.x))
+    obstaculos.forEach(obstaculo => {
+        if ((jugador.position.x+jugador.tamano.x) > obstaculo.position.x && jugador.position.x < (obstaculo.position.x+obstaculo.tamano.x)) {       
+            if (jugador.position.y+jugador.tamano.y > (obstaculo.position.y-10) ) {
+                console.log("choque")
+                this.perder();
+                booleanos.choque = true
+            }
+        }
+    });
+    booleanos.choque=false;
+}
 
 //
 var fechanow= Date.now();
 function animate(){
     window.requestAnimationFrame(animate)
-    
     c.clearRect(0,0,canvas.width,canvas.height)
+
+    c.fillStyle = "black";
+    c.shadowBlur = 1;
+    c.shadowColor = "black";  
+    c.globalAlpha = 0.8;
+    c.font = "15px Arial black";
+    c.fillText("Puntuacion: "+propiedades.puntuacion, 10, 20);
+    if (propiedades.puntuacion > propiedades.puntuacion_record) {
+        propiedades.puntuacion_record = propiedades.puntuacion
+    }
+    c.fillText("Puntuacion Record: "+propiedades.puntuacion_record, 10, 40);
+
+
     jugador.update();
     suelo.update();
     obstaculos.forEach((obstaculo) => {
@@ -289,24 +354,29 @@ function animate(){
     
     if (propiedades.objetos.velocidad_actual < propiedades.objetos.velocidad_maxima) {
         propiedades.objetos.velocidad_actual*= 1.001
-        console.log(propiedades.objetos.velocidad_actual)
+        //console.log(propiedades.objetos.velocidad_actual)
     }
     //console.log(jugador.velocidad.y)
     //console.log(booleanos.salto.permitido)
     if (teclas.arriba.pressed && booleanos.salto.permitido) {
-        jugador.velocidad.y *= -2
+        jugador.velocidad.y *= -3
+        
     } else if (teclas.abajo.pressed) {
         jugador.velocidad.y *= 4
     }
     if (teclas.left.pressed) {
-        jugador.velocidad.x +=-0.5
+        jugador.velocidad.x +=-0.7
     } else if (teclas.right.pressed){
-        jugador.velocidad.x +=0.5
+        jugador.velocidad.x +=0.7
     }
+    this.chocar()
+
     
     
     
 }
+
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
